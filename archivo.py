7421dfec348a2b9d2d134988e3139cafb33a1658
Matías Polero <MatiasPoleroCM@outlook.com>
2024-10-19 16:40:12 -0300

    Primer cambio

---
+++ archivo.py
@@ -0,0 +1,4 @@
+print('Hola! Mi nombre es Matias y mi apellido es Polero')
+
+print('Espero que tengas un bonito día')
+
---
+++ codigo_prueba.js
@@ -0,0 +1,15 @@
+// Función para sumar 2 números
+function sumar(a,b){
+    return a+b //Error intencional: debería de ser a + b
+}
+
+// Usando la función para sumar 2 números
+let numero1 = 5
+let numero2 = 2
+let numero3 = 6
+
+// Llamando a la función y almacenando el resultado
+let resultado = sumar (numero1,numero2)
+
+// Mostrar el resultado
+console.log("El resultado de sumar "+numero1+" y "+numero2+" es: "+resultado)
